d7ad70c066f751d2fb355fdc568765b631fc974e
vokomod <vokomod@gmail.com>
2018-06-15 01:57:44 +0300

    create db on app init

--- app.rb
+++ app.rb
@@ -1,6 +1,18 @@
 require 'rubygems'
 require 'sinatra'
 require 'pony'
+require "sqlite3"
+
+
+configure do
+	@db = SQLite3::Database.new 'barbershop.db'
+	@db.execute 'CREATE TABLE IF NOT EXISTS "users" ("id" INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL,
+    																								"name" TEXT,
+																										"phone" TEXT,
+																										"datestamp" TEXT,
+																										"barber" TEXT,
+																										"color" TEXT)'
+end
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"
